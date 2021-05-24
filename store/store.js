f54fb74d4de791db10abbc7c0290363da72b4612
stephanie-p8 <72699488+stephanie-p8@users.noreply.github.com>
2021-05-24 14:21:55 +0200

    add doc

--- store/store.js
+++ store/store.js
@@ -2,6 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import libraryReducer from "../components/library/librarySlice";
 import padReducer from "../components/pad/padSlice";
 
+/**
+ * Give reducers to store
+ * @author Stephanie PERAFAN
+ * @version 1.0.0
+ * @see {@link https://redux-toolkit.js.org/usage/usage-with-typescript#configurestore | Configure the store - Redux toolkit }
+ */
 export default configureStore({
   reducer: {
     library: libraryReducer,
